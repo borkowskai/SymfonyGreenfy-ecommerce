696256287fcd808d2000c675f99f93d49b9fbeff
Iza HP <you@example.com>
2020-04-09 23:33:35 +0200

    no working added color in flower

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -1,14 +1,19 @@
 <?php
 namespace App\Form;
 
-use PhpParser\Builder;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+// la classe pour avoir une entité comme champ du formulaire
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Color;
+
 
 class ProductType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -18,7 +23,12 @@ class ProductType extends AbstractType {
                 ->add('priceExclVAT', MoneyType::class)
                 ->add('reorderQuantity', IntegerType::class)
                 ->add('reorderLevel', IntegerType::class)
+                ->add('color', EntityType::class,[
+                    'class' => Color::class
+                ])
                 ->add('photo', FileType::class);
+
+
     }
     
 }
