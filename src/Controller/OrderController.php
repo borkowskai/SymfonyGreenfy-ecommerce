580dd19f5fb30bd7645aca6ece026904bceeca68
IB-HP <you@example.com>
2020-04-17 12:12:35 +0200

    inserted OrderLine in DB

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,18 +2,42 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Flower;
+use App\Entity\OrderLine;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OrderController extends AbstractController
 {
     /**
-     * @Route("/order", name="order")
+     * @Route("/order/add-orderLine", name="add-orderLine")
      */
-    public function index()
-    {
-        return $this->render('order/index.html.twig', [
-            'controller_name' => 'OrderController',
-        ]);
-    }
+    public function addOrderLine(Request $request){
+    
+
+        $id =$request->get('id');
+        $em = $this->getDoctrine()->getManager();
+        $flower = $em->getRepository(Flower::class)->findOneBy(array("id"=>$id));
+
+        // // // $flower->getPhoto();
+        // // // $flower->getPriceExclVAT();
+        // // $flower->getId();
+       
+        // // Création de l'entité OrderLine
+        $orderLine = new OrderLine();
+        // On lie le produit à l'ligne de l'ordre
+        $orderLine->setFlower( $flower);
+        $orderLine->setQuantity(1);
+        $orderLine->setActualPriceExclVAT($flower->getPriceExclVAT());
+
+
+        // // Étape 1 : On « persiste » l'entité
+        $em->persist($orderLine);
+
+        // // Étape 2 : On déclenche l'enregistrement
+        $em->flush();
+
+        return $this->render('front/shopping_cart.html.twig');
+        }
 }
